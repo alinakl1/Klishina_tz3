557bbd61cb7ec688478a95ebeb7aed8878b4ea89
alinakl1 <43699056+alinakl1@users.noreply.github.com>
2021-12-19 22:42:33 +0600

    Delete vyvod.py

--- vyvod.py
+++
@@ -1,9 +0,0 @@
-from main import maxim, summ, minim, proizv
-
-with open("input.txt", 'r') as file:
-    data = list(map(int, file.read().split()))
-
-print("максимум:", maxim(data))
-print("минимум:", minim(data))
-print("сумма:", summ(data))
-print("произведение:", proizv(data))
