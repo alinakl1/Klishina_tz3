0a1e97ce979483cfd9b4246e4ca05d20f9be6182
alinakl1 <43699056+alinakl1@users.noreply.github.com>
2021-12-19 23:01:59 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,35 @@
-def minim(num):
-    minn = num[0]
-    for el in num:
+def minim(ar):
+    minn = ar[0]
+    for el in ar:
         if el < minn:
             minn = el
     return minn
 
 
-def maxim(num):
-    maxn = num[0]
-    for el in num:
+def maxim(ar):
+    maxn = ar[0]
+    for el in ar:
         if el > maxn:
             maxn = el
     return maxn
 
 
-def summ(num):
+def summ(ar):
     s = 0
-    for el in num:
+    for el in ar:
         s = s + el
     return s
 
 
-def proizv(num):
+def proizv(ar):
     p = 1
     try:
-        for el in num:
+        for el in ar:
             p = p * el
     except OverflowError:
         p = "error"
     return p
 
 
-data = []
 with open("input.txt", 'r') as file:
     a = list(map(int, file.read().split()))
-
